11e75bcbb99ec2e43dae85df3e2b6d5771f729aa
VishVenu <vishakhasreejit@gmail.com>
2022-07-18 21:21:53 +0530

    Client class created and main method

--- BankingSystem/src/Banking.java
+++ BankingSystem/src/Banking.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class Banking {
+    static Scanner in = new Scanner(System.in);
+
+    public static Client endUser;
+    public static void main(String[] args) {
+        showWelcomeScreen();
+        if(endUser!=null){
+            showPostLoginMenu();
+        }else{
+            showPreLoginMenu();
+        }
+        int input = in.nextInt();
+    }
+
+    public static void showWelcomeScreen(){
+        System.out.println("///////////////////////////////////////////");
+        System.out.println("--------Welcome to Banking System-----------");
+        System.out.println("///////////////////////////////////////////");
+    }
+
+    public static void showPreLoginMenu(){
+        System.out.println("***************************************************");
+        System.out.println("Please select one of the below options:");
+        System.out.println("1. New user registration");
+        System.out.println("2. Login user");
+        System.out.println("3. Reset password");
+        System.out.println("4. Exit");
+        System.out.println("***************************************************");
+    }
+
+    public static void showPostLoginMenu(){
+        System.out.println("***************************************************");
+        System.out.println("Please select one of the below options:");
+        System.out.println("1. Update user details");
+        System.out.println("2. Create new banking account");
+        System.out.println("3. Perform banking transactions");
+        System.out.println("4. Exit");
+        System.out.println("***************************************************");
+    }
 }
--- BankingSystem/src/Client.java
+++ BankingSystem/src/Client.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Client {
+    public int clientID;
+    public ArrayList<Integer> accountID;
+    public  String name;
+    public int phoneNumber;
+    public String email;
+    public String password;
+    public String securityQuestion;
+    public String securityAnswer;
+    public String postalCode;
+    public Date dateOfBirth;
+
+    public void createUser(){
+
+    }
+
+    public void UpdateUser(){
+
+    }
+
+    public void resetPassword(){
+
+    }
+
+    public void createAccount(){
+
+    }
+
+    public void loginUser(){
+
+    }
+
+    public void performBankingTransactions(){
+
+    }
 }
