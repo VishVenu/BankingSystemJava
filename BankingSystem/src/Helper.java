fa40611b3e2aab6f8eb7f041faaeddd8ca000ad0
Shijith_k <38531459+sh1j1th@users.noreply.github.com>
2022-07-19 10:43:36 -0400

    set client data in csv

--- BankingSystem/src/Helper.java
+++ BankingSystem/src/Helper.java
@@ -1,6 +1,75 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class Helper {
+    static String clientCSVFilePath = "BankingSystem/src/clients.csv";
+    private static List<List<String>> readFromCSV(String filePath) {
+        List<List<String>> records = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+
+            while (scanner.hasNextLine()) {
+                records.add(getRecordFromLine(scanner.nextLine()));
+            }
+
+            return records;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found! " + filePath);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(",");
+            while (rowScanner.hasNext()) {
+                values.add(rowScanner.next());
+            }
+        }
+        return values;
+    }
+
+    public static void getClientData() {
+
+        List<List<String>> transactions = readFromCSV(clientCSVFilePath);
+    };
+
+    public static void setClientData(String name, String phoneNumber, String emailID, String password,
+                                     String securityQuestion, String securityAnswer, String postalCode,
+                                     Date dateOfBirth) {
+        try {
+            // create a list of objects
+            List<String> client = Arrays.asList(name, phoneNumber, emailID,
+                    String.valueOf(dateOfBirth), password,
+                    securityQuestion, securityAnswer, postalCode);
+
+            File file = new File(clientCSVFilePath);
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            BufferedWriter writer = Files.newBufferedWriter(Path.of(clientCSVFilePath));
+
+            // header
+            writer.write("Name,PhoneNumber,EmailID,DateOfBirth,Password,SecurityQuestion,SecurityAnswer,PostalCode");
+            writer.newLine();
+
+            // write data
+            writer.write(String.join(",", client));
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static String updateAccountData(String accountData){
         return "test";
     }
 }
+
