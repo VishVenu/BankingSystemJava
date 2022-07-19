7c201e40ce64657f6f8ce4395b13edb682da097e
Parwat Kunwar <parwatkunwar08@gmail.com>
2022-07-19 08:59:02 -0400

    Add account class methods for transactions

--- BankingSystem/src/Account.java
+++ BankingSystem/src/Account.java
@@ -1,2 +1,17 @@
+import java.time.LocalDateTime;
+
 public class Account {
+
+    public static void addTransaction(double amount, String eventDescription, TransactionType eventType, String transactionID) {
+        LocalDateTime date = LocalDateTime.now();
+
+
+        Helper.setTransactionData(amount,eventDescription,eventType,transactionID,date);
+
+    }
+
+    public static void getTransactionHistory() {
+        // Lists the last 3 transactions.
+    }
+
 }
