4362c57637a02ef6b6d59bb76ed5970ed2ce4c68
Shijith_k <38531459+sh1j1th@users.noreply.github.com>
2022-07-19 00:26:13 -0400

    minor changes added opencsv

--- BankingSystem/src/Client.java
+++ BankingSystem/src/Client.java
@@ -1,12 +1,18 @@
+import com.opencsv.bean.util.OpencsvUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Client {
+
     public int clientID;
     public ArrayList<Integer> accountID;
     public  String name;
-    public int phoneNumber;
+    public String phoneNumber;
     public String email;
     public String password;
     public String securityQuestion;
@@ -14,11 +20,34 @@ public class Client {
     public String postalCode;
     public Date dateOfBirth;
 
-    public void createUser(){
+    Scanner in = new Scanner(System.in);
+
+    public void createUser() throws ParseException {
+        System.out.println("****Please follow the steps to register as a new user****");
+        System.out.print("Enter fullname ");
+        name = in.nextLine();
+        System.out.print("Enter phone number: ");
+        phoneNumber = in.nextLine();
+        System.out.print("Enter emailID: ");
+        email = in.nextLine();
+        System.out.print("Enter postal code: ");
+        postalCode = in.nextLine();
+        System.out.print("Enter date of birth(dd/MM/yyyy): ");
+        dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(in.nextLine());
+        System.out.print("Enter password: ");
+        password = in.nextLine();
+        System.out.print("Enter security question: ");
+        securityQuestion = in.nextLine();
+        System.out.print("Enter security answer: ");
+        securityAnswer = in.nextLine();
+
+        System.out.println("Enter Y to proceed or any other key to return to home screen.");
+        String choice = in.nextLine();
+        //choice.toUpperCase() == "Y" ? setUserData() && Banking.main() : Banking.main();
 
     }
 
-    public void UpdateUser(){
+    public void updateUser(){
 
     }
 
@@ -27,7 +56,26 @@ public class Client {
     }
 
     public void createAccount(){
+        System.out.println("***************************************************");
+        System.out.println("Please select one of the below account type:");
+        System.out.println("1. Chequing");
+        System.out.println("2. Savings");
+        System.out.println("3. Exit");
+        System.out.println("***************************************************");
+        String accountTypeChoice = in.nextLine();
+        switch (accountTypeChoice){
+            case "1": //chequing
+                System.out.println("Account created successfully.");
 
+                //
+                String accountNumber = Helper.updateAccountData("String"); // must generate a random
+                //id and sent it back to user, also must append to the
+                //list too
+                System.out.printf("Account number: %S", accountNumber);
+                break;
+            case "2": //test
+                break;
+        }
     }
 
     public void loginUser(){
@@ -37,4 +85,5 @@ public class Client {
     public void performBankingTransactions(){
 
     }
+
 }
--- BankingSystem/src/Helper.java
+++ BankingSystem/src/Helper.java
@@ -1,2 +1,6 @@
 public class Helper {
+
+    public static String updateAccountData(String accountData){
+        return "test";
+    }
 }
